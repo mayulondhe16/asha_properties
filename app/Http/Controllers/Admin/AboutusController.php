d4822ab8af58e8beff538131f24b5971bc6f40ce
mayulondhe16 <mayulondhe16@gmail.com>
2023-09-04 13:08:01 +0530

    image update issue resolved

--- app/Http/Controllers/Admin/AboutusController.php
+++ app/Http/Controllers/Admin/AboutusController.php
@@ -123,8 +123,14 @@ class AboutusController extends Controller
                 }
 
             }
-
-            $fileName = $id."_updated.". $request->image->extension();
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                $randomString = '';
+            
+                for ($i = 0; $i < 10; $i++) {
+                    $index = rand(0, strlen($characters) - 1);
+                    $randomString .= $characters[$index];
+                }
+            $fileName = $randomString.".". $request->image->extension();
             uploadImage($request, 'image', $path, $fileName);
            
             $brand = Aboutus::find($id);
--- app/Http/Controllers/Admin/SocialmedialinksController.php
+++ app/Http/Controllers/Admin/SocialmedialinksController.php
@@ -113,8 +113,14 @@ class SocialmedialinksController extends Controller
                 }
 
             }
-
-            $fileName = $id.".". $request->image->extension();
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $randomString = '';
+        
+            for ($i = 0; $i < 10; $i++) {
+                $index = rand(0, strlen($characters) - 1);
+                $randomString .= $characters[$index];
+            }
+            $fileName = $randomString.".". $request->image->extension();
             uploadImage($request, 'image', $path, $fileName);
             $socialmedialinks->image = $fileName;
 
